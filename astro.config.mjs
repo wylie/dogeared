b8eaa580b8da2f11631724836ee47ac6cf0c913a
Wylie Fisher <me@wyliefisher.com>
2026-04-14 19:58:55 -0400

    Use current @astrojs/vercel adapter import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
